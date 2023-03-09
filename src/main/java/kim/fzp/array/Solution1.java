1352d272cc8ce49952f9d7b9c653f8633ad117c9
395656356 <fzp@hibobi.com>
2023-03-09 16:07:15 +0800

    twoSum

---
+++ src/main/java/kim/fzp/array/Solution1.java
@@ -0,0 +1,28 @@
+package kim.fzp.array;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * 第1题：两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * author fzp
+ * date 2023/3/9 15:42
+ */
+public class Solution1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+
+        }
+
+        return new int[0];
+    }
+}
